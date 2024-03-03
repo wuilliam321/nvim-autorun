f087c05a1339e3108816f16755fb197639441275
Wuilliam Lacruz <wuilliam.lacruz@mercadolibre.com>
2024-03-02 23:32:56 -0300

    fixes and tests fails now works

--- plugin/go-tests.lua
+++ plugin/go-tests.lua
@@ -1,40 +1,10 @@
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then k = '"' .. k .. '"' end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
-
-function P(o)
-    return print(dump(o))
-end
-
 local parsers = require('nvim-treesitter.parsers')
--- local query = require('nvim-treesitter.query')
 local ts = vim.treesitter
--- local ts_utils = require 'nvim-treesitter.ts_utils'
 local ns = vim.api.nvim_create_namespace("go-tests")
 
 local tests = {}
 
-local find_lines = function(bufnr)
-    local root
-    local parser = parsers.get_parser(bufnr, "go")
-    local first_tree = parser:trees()[1]
-
-    if first_tree then
-        root = first_tree:root()
-    end
-    local start_row, _, end_row, _ = root:range()
-
-    -- local root = parser:parse()[1]
-    local query = ts.query.parse("go", [[
+local function_names_query = [[
     (package_clause (package_identifier) @package)
     (function_declaration
         name: (identifier) @func_name
@@ -57,16 +27,23 @@ local find_lines = function(bufnr)
             )
         )?
     )
-    ]])
+]]
 
-
+local find_lines = function(bufnr)
+    local root
+    local parser = parsers.get_parser(bufnr, "go")
+    local first_tree = parser:trees()[1]
+    if first_tree then
+        root = first_tree:root()
+    end
+    local start_row, _, end_row, _ = root:range()
+    local query = ts.query.parse("go", function_names_query)
     local names = {}
     local pkg = ""
     for _, match, _ in query:iter_matches(root, bufnr, start_row, end_row) do
         local func_name
         for id, node in pairs(match) do
             local name = query.captures[id]
-
             if name == "func_name" then
                 local line = node:range()
                 local pattern = "[^%w\']+"
@@ -75,7 +52,6 @@ local find_lines = function(bufnr)
                 func_name = func_name:gsub(pattern, "_")
                 table.insert(names, { name = func_name, line = line })
             end
-
             if name == "func_sname" then
                 local line = node:range()
                 local pattern = "[^%w\']+"
@@ -84,35 +60,102 @@ local find_lines = function(bufnr)
                 func_sname = func_sname:gsub(pattern, "_")
                 table.insert(names, { name = func_name .. "/" .. func_sname, line = line })
             end
-
             if name == "package" and pkg == "" then
                 pkg = ts.get_node_text(node, 0)
             end
         end
     end
-
     for _, v in pairs(names) do
         local key = pkg .. "/" .. v.name
         if key and tests[key] then
             tests[key].line = v.line
         end
     end
-    -- print("tests", vim.inspect(tests))
 end
 
 local show_results = function(bufnr)
     vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+    vim.diagnostic.reset(ns, bufnr)
+    local diaginostics = {}
     for _, test in pairs(tests) do
         if test.success and test.line then
-            local text = { "✓", "AlphaShorcut" }
+            local text = { "✓ Test pass", "@string" }
             vim.api.nvim_buf_set_extmark(bufnr, ns, test.line, 0, {
                 virt_text = { text },
             })
         end
+        if test.failed and test.line then
+            local d = {
+                bufnr = bufnr,
+                lnum = test.line,
+                col = 0,
+                message = "✗ Test failed",
+                severity = vim.diagnostic.severity.ERROR,
+            }
+            table.insert(diaginostics, d)
+        end
     end
-    -- print("tests", vim.inspect(tests))
+    if #diaginostics > 0 then
+        print("d", vim.inspect(diaginostics))
+        vim.diagnostic.set(ns, bufnr, diaginostics, {})
+    end
 end
 
+local set_success_test = function(decoded)
+    local key
+    local pkg
+    if decoded.Package ~= nil and decoded.Test ~= nil then
+        local parts = vim.split(decoded.Package, "/")
+        pkg = parts[#parts]
+        key = pkg .. "/" .. decoded.Test
+    end
+    if key ~= nil then
+        tests[key] = {
+            key = key,
+            name = decoded.Test,
+            package = pkg,
+            success = true,
+            failed = false,
+        }
+    end
+end
+
+local set_failed_test = function(decoded)
+    local key
+    local pkg
+    if decoded.Package ~= nil and decoded.Test ~= nil then
+        local parts = vim.split(decoded.Package, "/")
+        pkg = parts[#parts]
+        key = pkg .. "/" .. decoded.Test
+    end
+    if key ~= nil then
+        tests[key] = {
+            key = key,
+            name = decoded.Test,
+            package = pkg,
+            success = false,
+            failed = true,
+        }
+    end
+end
+
+local output_handler = function(_, data)
+    for _, line in ipairs(data) do
+        if not data then
+            return
+        end
+        if line == "" then
+            return
+        end
+        local decoded = vim.json.decode(line)
+        if decoded.Action == "pass" then
+            set_success_test(decoded)
+        end
+        if decoded.Action == "fail" then
+            set_failed_test(decoded)
+        end
+    end
+end
 
 local execute = function(command, handler)
     vim.fn.jobstart(command, {
@@ -128,46 +171,11 @@ local execute = function(command, handler)
     })
 end
 
-local output_handler = function(_, data)
-    for _, line in ipairs(data) do
-        if not data then
-            return
-        end
-
-        if line == "" then
-            return
-        end
-        local decoded = vim.json.decode(line)
-
-        if decoded.Action == "pass" then
-            local key
-            local pkg
-            if decoded.Package ~= nil and decoded.Test ~= nil then
-                local parts = vim.split(decoded.Package, "/")
-                pkg = parts[#parts]
-                key = pkg .. "/" .. decoded.Test
-            end
-            if key ~= nil then
-                tests[key] = {
-                    key = key,
-                    name = decoded.Test,
-                    package = pkg,
-                    success = true,
-                }
-            end
-        end
-    end
-end
-
-
 vim.api.nvim_create_user_command("GoTests", function()
     local group = vim.api.nvim_create_augroup("WL", { clear = true })
     local bufnr = vim.api.nvim_create_buf(false, true)
-    -- local command = vim.fn.split(vim.fn.input("Command> ", "go test ./... -json"), " ")
-    -- local pattern = vim.fn.input("Pattern> ", "*.go")
-    local command = vim.fn.split("go test ./... -json", " ")
+    local command = vim.fn.split("go test ./... -json -short", " ")
     local pattern = "*.go"
-
     vim.api.nvim_create_autocmd("BufWritePost", {
         group = group,
         pattern = pattern,
